25d756f55d9d71112e5400997d06f84ece395025
Jose Roman <el-1ero@outlook.com>
2025-04-09 23:29:01 -0400

    First commit

---
+++ src/app/dashboard/counter/page.tsx
@@ -0,0 +1,18 @@
+import { CartCounter } from "@/shopping-cart";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Shopping Cart",
+  description: "Un simple contador",
+};
+
+const CounterPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      <CartCounter value={20} />
+    </div>
+  );
+};
+
+export default CounterPage;
